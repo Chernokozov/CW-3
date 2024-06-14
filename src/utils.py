83463da86904e6da82f52d4abe9b55a12110863c
chernokozov <chernokozovyu@gmail.com>
2024-06-14 11:57:45 +0300

    Создание экземпляров класса, добавление файла json, настройка путей до файла json

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 
+from src.operations import Operation
+
 
 def load_operations_from_json(path):
     """
@@ -10,3 +12,24 @@ def load_operations_from_json(path):
     """
     with open(path, encoding="utf-8") as file:
         return json.load(file)
+
+
+def load_operations_instances(operations):
+    """
+    Создает экземпляры класса Operation из списка операций
+    :param operations:
+    :return:
+    """
+    return [
+        Operation(
+            date=operation["date"],
+            state=operation["state"],
+            amount=operation["amount"],
+            currency_name=operation["currency_name"],
+            description=operation["description"],
+            from_account=operation["from"],
+            to_account=operation["to"]
+        )
+        for operation in operations
+        if operation
+    ]
